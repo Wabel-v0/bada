bb480cb85787cc966fdeaf3d8acdcef20ef963cd
mohalw44 <mohalw04@gmail.com>
2025-05-01 18:20:04 +0300

    added token

---
+++ token/token.go
@@ -0,0 +1,32 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// Idenfiers and Literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "ASSIGN"
+	PLUS   = "PLUS"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
